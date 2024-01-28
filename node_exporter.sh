0512133d7ef7bbd710791d040e166375295350ee
rashenko <149801050+rashenko@users.noreply.github.com>
2024-01-28 18:25:42 +0300

    node_exporter.sh

--- node_exporter.sh
+++ node_exporter.sh
@@ -14,7 +14,7 @@ cp node_exporter-*.linux-amd64/node_exporter /usr/local/bin
 chown node_exporter: /usr/local/bin/node_exporter
 
 # Копируем конфиг службы node exporter
-cp /home/qqee/nginx-git/node_exporter.service /etc/systemd/system/node_exporter.service
+cp /home/qqee/backend1-git/node_exporter.service /etc/systemd/system/node_exporter.service
 
 # Перезапуск
 systemctl daemon-reload
