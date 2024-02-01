5e8b1421d153544ca869cfe0f67eef91fea67d4d
rashenko <149801050+rashenko@users.noreply.github.com>
2024-02-01 18:01:25 +0300

    apache_script_b1.sh

--- apache_script_b1.sh
+++ apache_script_b1.sh
@@ -3,13 +3,13 @@
 DIR='/etc/apache2/sites-available/'
 DIR1='/home/qqee/backend1-git/000-backend1.conf'
 DIR2='/etc/apache2/sites-available/000-backend1.conf'
-#DIR4='/var/www/html/index.html'
-#DIR5='/var/www/backend1/'
-#DIR6='/var/www/backend1/index.html'
+DIR4='/var/www/html/index.html'
+DIR5='/var/www/backend1/'
+DIR6='/var/www/backend1/index.html'
 cp $DIR1 $DIR2
 echo "Конфиг 000-backend1.conf, Apache сервиса скопирован по пути $DIR"
-#mkdir $DIR5
-#cp $DIR4 $DIR6
+mkdir $DIR5
+cp $DIR4 $DIR6
 
 systemctl restart apache2
 if [ $? -eq 0 ]; then
